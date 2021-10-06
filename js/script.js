6e7f6dc6085b2cfdba4948fcc1c94c798584fd9a
Sistania Bong <sistaniameilina@gmail.com>
2021-10-05 23:01:00 -0400

    add today weather, five day weather, and recent search display

--- js/script.js
+++ js/script.js
@@ -1,31 +1,44 @@
 var cityFormEl = $('#user-form'); //form element
 var cityInputEl = $('#city'); //form city input elemet
-var todayWeatherContainer = $('.today-container'); //container to display today weather
-var todayWeatherEl = $('#today-weather'); //element to display today weather
+var WeatherContainer = $('.weather-container'); //container to display today weather
+var todayWeatherEl = $('.today-weather'); //element to display today weather
+var fiveDayWeatherContainer = $('.five-day-weather-container'); //element to display today weather
+var fiveDayWeatherEl = $('.five-day-weather');
+var fiveDayHeaderEl = $('.five-day-header');
+var RecentSearch =  $('.card-recent');
 var APIKey = '6f092801568446709287b96be65ed97a';
 
 
 
-// function formSubmitHandler(event){
-//     event.preventDefault();
-//     var searchValue = inputBox.val();
-//     console.log(searchValue)
+function init(){
+    renderSearchedCities();
+}
 
-//     requestURL = genericApiUrl + 'q=' + searchValue + '&fo=json'
-//     console.log(requestURL)
+function renderSearchedCities(){
+    var cities = JSON.parse(localStorage.getItem('cities'));
+
+    if (cities){
+        for (var i=0;i<cities.length;i++){
+            var city = $('<button>').text(cities[i]).attr({'class':'btn', 'id': cities[i]});
+            RecentSearch.append(city);
+            $('#'+ cities[i]).on('click',recentSearchHandler);
+        }
+    }   
+}
+
+
+var recentSearchHandler = function () {
+  
+    console.log('city to search for: ' + $(this).text());
+    var city = $(this).text();
     
-//     fetch (requestURL)
-//         .then(function (response){
-//             return response.json()
-//         })
-//         .then(function(data){
-//             console.log(data)
-//         });  
-//         var searchEl = document.createElement('a');
-//         // repoEl.classList = 'list-item flex-row justify-space-between align-center';
-//         searchEl.setAttribute('href', './search-results.html?search=' + searchValue);
-// }
-
+    if (city) {
+        todayWeatherEl.textContent = '';
+        cityInputEl.val('');
+        getTodayWeather(city);
+    }
+    return;
+};
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -42,8 +55,10 @@ var formSubmitHandler = function (event) {
     }
 };
 
+
+
 var getTodayWeather = function (city) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey +"&units=imperial";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
   
     fetch(queryURL)
       .then(function (response) {
@@ -53,8 +68,8 @@ var getTodayWeather = function (city) {
             var lat = data.coord.lat;
             var lon = data.coord.lon;
             displayTodayWeather(data);
-            // getFutureWeather(lat,lon)
-            // saveRecentSearch(city)
+            getFutureWeather(lat,lon);
+            saveRecentSearch(city)
           });
         } else {
           alert('Error: ' + response.statusText);
@@ -65,27 +80,82 @@ var getTodayWeather = function (city) {
       });
   };
 
+function saveRecentSearch(city){
+    const searchedCity = (() => {
+        const cities = localStorage.getItem('cities');
+        return cities === null ? []: JSON.parse(cities);
+      })();
+    
+    //   add activity in the local storage
+    if (!(searchedCity.includes(city))){
+        searchedCity.push(city)
+    }
+
+    localStorage.setItem("cities", JSON.stringify(searchedCity));
+}
+
+
 function displayTodayWeather(data) {
     var weatherDisplayAttr = {'color':'white', 'font-weight': '500', 'font-size': '1.2rem'};
-    todayWeatherContainer.css({'border': '3px solid #466786','border-radius': '.3rem', 'background-color':'rgb(15, 144, 153)', 'height': '72%', 'padding': '10px'});
+    // todayWeatherContainer.css({'border': '3px solid #466786','border-radius': '.3rem', 'background-color':'rgb(15, 144, 153)', 'height': '72%', 'padding': '10px'});
+    todayWeatherEl.css({'border': '3px solid #466786','border-radius': '.3rem', 'background-color':'rgb(15, 144, 153)', 'height': '72%', 'padding': '10px'});
+
     var dateString = moment.unix(data.dt).format('dddd, MMMM Do YYYY');
     todayWeatherEl.append($('<h3>').text(data.name + ' - ' + dateString).css({'color':'white'}));
     var iconcode = data.weather[0].icon;
     var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    todayWeatherContainer.append($('<img>').attr({'src': iconurl, 'id': 'wicon', 'alt':'Weather Icon', 'display': 'inline', 'width':'100px', 'float': 'left'}));
-    todayWeatherContainer.append($('<h3>').text(data.weather[0].description).css({'color':'white', 'font-weight': '500', 'font-size': '1.2rem', 'display':'inline-block'}));
-    todayWeatherContainer.append($('<h3>').text('Temperature: ' + data.main.temp + ' ℉').css(weatherDisplayAttr));
-    todayWeatherContainer.append($('<h3>').text('Feels Like: ' + data.main.feels_like + ' ℉').css(weatherDisplayAttr));
-    todayWeatherContainer.append($('<h3>').text('Wind: ' + data.wind.speed +' MPH').css(weatherDisplayAttr));
-    todayWeatherContainer.append($('<h3>').text('Humidity: ' + data.main.humidity +'%').css(weatherDisplayAttr));
+    todayWeatherEl.append($('<img>').attr({'src': iconurl, 'id': 'wicon', 'alt':'Weather Icon', 'display': 'inline', 'width':'100px', 'float': 'left'}));
+    todayWeatherEl.append($('<h3>').text(data.weather[0].description).css({'color':'white', 'font-weight': '500', 'font-size': '1.2rem', 'display':'inline-block'}));
+    todayWeatherEl.append($('<h3>').text('Temperature: ' + data.main.temp + ' ℉').css(weatherDisplayAttr));
+    todayWeatherEl.append($('<h3>').text('Feels Like: ' + data.main.feels_like + ' ℉').css(weatherDisplayAttr));
+    todayWeatherEl.append($('<h3>').text('Wind: ' + data.wind.speed +' MPH').css(weatherDisplayAttr));
+    todayWeatherEl.append($('<h3>').text('Humidity: ' + data.main.humidity +'%').css(weatherDisplayAttr));
     var sunriseTime = moment.unix(data.sys.sunrise).format('h:mm A');
     var sunsetTime = moment.unix(data.sys.sunset).format('h:mm A');
-    todayWeatherContainer.append($('<h3>').text('Sunrise: ' + sunriseTime).css(weatherDisplayAttr));
-    todayWeatherContainer.append($('<h3>').text('Sunset: ' + sunsetTime).css(weatherDisplayAttr));
+    todayWeatherEl.append($('<h3>').text('Sunrise: ' + sunriseTime).css(weatherDisplayAttr));
+    todayWeatherEl.append($('<h3>').text('Sunset: ' + sunsetTime).css(weatherDisplayAttr));
 };
 
+function getFutureWeather(lat,lon){
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey +"&exclude=minutely,hourly,alerts" + "&units=imperial";
+        
+    fetch(queryURL)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+            displayFiveDayWeather(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+}
+
+function displayFiveDayWeather(data){
+    var dailyWeather = data.daily;
+    console.log(dailyWeather);
+    fiveDayHeaderEl.append($('<h3>').text('5-Day Forecast'));
+    for (var i=1;i<6;i++){
+        var dateString = moment.unix(dailyWeather[i].dt).format('MMMM Do YYYY');
+        var divWeather = $('<div>').addClass('col-sm');
+        divWeather.css({'border': '2px solid #466786','border-radius': '.3rem', 'background-color':'#c5ecf3', 'height': '72%', 'padding': '10px'});
+        divWeather.append($('<h3>').text(dateString).css({'font-size': '1rem', 'font-weight':'500'}));
+        var iconcode = dailyWeather[i].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        divWeather.append($('<img>').attr({'src': iconurl, 'id': 'wicon', 'alt':'Weather Icon', 'width':'100px'}));
+        divWeather.append($('<h3>').text('Temp: ' + dailyWeather[i].temp.day + '℉').css({'font-size': '1rem', 'font-weight':'200'}))
+        divWeather.append($('<h3>').text('Wind: ' + dailyWeather[i].wind_speed + 'MPH').css({'font-size': '1rem', 'font-weight':'200'}))
+        divWeather.append($('<h3>').text('Humidity: ' + dailyWeather[i].humidity + '%').css({'font-size': '1rem', 'font-weight':'200'}))       
+        fiveDayWeatherEl.append(divWeather)
+    }
+}
 
 
 
 cityFormEl.on("submit",formSubmitHandler);
 
+init();
